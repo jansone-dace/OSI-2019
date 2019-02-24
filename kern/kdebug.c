a94a23c632a5d96f5063e178974cbf86b02a708d
Dace Jansone <jansone-dace@inbox.lv>
2019-02-24 12:50:21 +0200

    Exercise 12

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -179,6 +179,15 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	//	Look at the STABS documentation and <inc/stab.h> to find
 	//	which one.
 	// Your code here.
+	stab_binsearch(stabs, &lline, &rline, N_SLINE, addr);
+
+	if (lline <= rline) {
+		// found
+		info->eip_line = stabs[rline].n_desc;	
+	} else {
+		// not found, return -1
+		return -1;
+	}
 
 
 	// Search backwards from the line number for the relevant filename
