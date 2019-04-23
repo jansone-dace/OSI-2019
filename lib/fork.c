0f0316b619dbf9348df50e0c89fd082ef25e2bf5
Dace Jansone <jansone-dace@inbox.lv>
2019-04-23 14:42:33 +0300

    Exercise 12

--- lib/fork.c
+++ lib/fork.c
@@ -7,6 +7,8 @@
 // It is one of the bits explicitly allocated to user processes (PTE_AVAIL).
 #define PTE_COW		0x800
 
+extern void _pgfault_upcall(void);
+
 //
 // Custom page fault handler - if faulting page is copy-on-write,
 // map in our own private writable copy.
@@ -25,6 +27,9 @@ pgfault(struct UTrapframe *utf)
 	//   (see <inc/memlayout.h>).
 
 	// LAB 4: Your code here.
+	//tf->tf_err & 2 == rakstisana
+	if (!((err & 2) && (uvpt[PGNUM(addr)] & PTE_COW)))
+		panic("access is not a write or not to a copy-on-write-page");
 
 	// Allocate a new page, map it at a temporary location (PFTEMP),
 	// copy the data from the old page to the new page, then move the new
@@ -34,8 +39,18 @@ pgfault(struct UTrapframe *utf)
 	//   No need to explicitly delete the old page's mapping.
 
 	// LAB 4: Your code here.
+	if ((r = sys_page_alloc(0, PFTEMP, PTE_U | PTE_P | PTE_W)) < 0)
+		panic("pgfault cannot allocate new writable page");
 
-	panic("pgfault not implemented");
+	memcpy(PFTEMP, ROUNDDOWN(addr, PGSIZE), PGSIZE);	
+
+	if((r = sys_page_map(0, PFTEMP, 0, ROUNDDOWN(addr, PGSIZE), PTE_U | PTE_P | PTE_W)) < 0)
+		panic("pgfault cannot copy date to the new page");
+
+	if ((r = sys_page_unmap(0, PFTEMP)) < 0)
+		panic("pgfault cannot unmap the old page");
+
+	//panic("pgfault not implemented");
 }
 
 //
@@ -55,7 +70,32 @@ duppage(envid_t envid, unsigned pn)
 	int r;
 
 	// LAB 4: Your code here.
-	panic("duppage not implemented");
+	//panic("duppage not implemented");
+
+	// duppage should map the page copy-on-write into the address space of the child 
+	// and then remap the page copy-on-write in its own address space. 
+	// duppage sets both PTEs so that the page is not writeable, 
+	// and to contain PTE_COW in the "avail" field to distinguish copy-on-write pages 
+	// from genuine read-only pages.
+
+
+	// If the page is writable or copy-on-write, the new mapping must be created copy-on-write,
+	// and then our mapping must be marked copy-on-write as well.
+	if (uvpt[pn] & PTE_W || uvpt[pn] & PTE_COW) {
+		// map the child
+		if ((r = sys_page_map(thisenv->env_id, (void *)(pn*PGSIZE), envid, (void *)(pn*PGSIZE), PTE_COW | PTE_U | PTE_P)) < 0)
+		panic("duppdage cannot map the child page: %e", r);
+
+		// map current environment
+		if ((r = sys_page_map(thisenv->env_id, (void *)(pn*PGSIZE), thisenv->env_id, (void *)(pn*PGSIZE), PTE_COW | PTE_U | PTE_P)) < 0)
+		panic("duppdage cannot map the current environment page: %e", r);
+	}
+	else {
+		// Do not give COW permissions
+		if ((r = sys_page_map(thisenv->env_id, (void *)(pn*PGSIZE), envid, (void *)(pn*PGSIZE), PTE_U | PTE_P)) < 0)
+		panic("duppdage cannot map the child page: %e", r);
+	}
+
 	return 0;
 }
 
@@ -79,7 +119,51 @@ envid_t
 fork(void)
 {
 	// LAB 4: Your code here.
-	panic("fork not implemented");
+	//panic("fork not implemented");
+	envid_t child_id;
+	int r;
+
+	// 1. The parent installs pgfault() as the C-level page fault handler, 
+	// using the set_pgfault_handler() function you implemented above.
+	set_pgfault_handler(pgfault);
+
+	// 2. The parent calls sys_exofork() to create a child environment.
+	child_id = sys_exofork();
+
+	if (child_id < 0)
+		panic("fork cannot create child environment: %e", child_id);
+
+	if (child_id == 0) {
+		// If this is the child process, fix "thisenv"
+		thisenv = &envs[ENVX(sys_getenvid())];
+		return 0;
+	}
+
+	// 3. For each writable or copy-on-write page in its address space below UTOP, 
+	// the parent calls duppage
+	// fork() also needs to handle pages that are present, but not writable or copy-on-write.
+	int i;
+	for (i = 0; i < USTACKTOP; i+= PGSIZE) {
+		if (((uvpd[PDX(i)] & PTE_P) == PTE_P) && ((uvpt[PGNUM(i)] & PTE_P) == PTE_P))
+			duppage(child_id, PGNUM(i));
+	}	
+	
+	//   Neither user exception stack should ever be marked copy-on-write,
+	//   so you must allocate a new page for the child's user exception stack.
+	if ((r = sys_page_alloc(child_id, (void *)(UXSTACKTOP - PGSIZE), PTE_U | PTE_P | PTE_W)) < 0)
+		panic("fork cannot allocate page for child's user exception stack: %e", r);
+	
+	
+	// 4. The parent sets the user page fault entrypoint for the child to look like its own.
+	if ((r = sys_env_set_pgfault_upcall(child_id, _pgfault_upcall)) < 0)
+		panic("fork cannot set user fault entrypoint for the child: %e", r);
+
+
+	// 5. The child is now ready to run, so the parent marks it runnable.
+	if ((r = sys_env_set_status(child_id, ENV_RUNNABLE)) < 0)
+		panic("fork cannot set child environment as runnable: %e", r);
+
+	return child_id;
 }
 
 // Challenge!
